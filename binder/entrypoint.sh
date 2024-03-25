db6a436cf300af62dc55d33d671101ad36f31b8a
MALINENI SRIKANTH <87704093+Srikanth635@users.noreply.github.com>
2024-03-25 16:45:37 +0100

    Update entrypoint.sh

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #source ${ROS_WS}/devel/setup.bash
-echo "source ${ROS_WS}/devel/setup.bash" >> ${HOME}/.bashrc
-echo "source ${KNOWROB_WS}/devel/setup.bash" >> ${HOME}/.bashrc
+#echo "source ${ROS_WS}/devel/setup.bash" >> ${HOME}/.bashrc
+#echo "source ${KNOWROB_WS}/devel/setup.bash" >> ${HOME}/.bashrc
 
 
 roscore &
@@ -17,7 +17,7 @@ mkdir -p ${PWD}/mongodb/data
 mongod --fork --logpath ${PWD}/mongodb/mongod.log --dbpath ${PWD}/mongodb/data
 
 # Launch Knowrob
-source ${KNOWROB_WS}/devel/setup.bash
+#source ${KNOWROB_WS}/devel/setup.bash
 export KNOWROB_MONGODB_URI=${MONGODB_URL}/?appname=knowrob
 roslaunch --wait knowrob knowrob.launch &
 

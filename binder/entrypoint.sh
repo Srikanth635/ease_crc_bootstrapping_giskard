c5094d466879f68a5b1eaa15540b89b64540ec91
Srikanth Malineni <srikanthmalineni635@gmail.com>
2024-03-26 09:49:52 +0100

    updated entrypoint.sh

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -14,8 +14,9 @@ mkdir -p ${PWD}/mongodb/data
 mongod --fork --logpath ${PWD}/mongodb/mongod.log --dbpath ${PWD}/mongodb/data
 
 # Launch Knowrob
-#source ${KNOWROB_WS}/devel/setup.bash
-#export KNOWROB_MONGODB_URI=${MONGODB_URL}/?appname=knowrob
-#roslaunch --wait knowrob knowrob.launch &
+source ${KNOWROB_WS}/devel/setup.bash
+export KNOWROB_MONGODB_URI=${MONGODB_URL}/?appname=knowrob
+roslaunch --wait knowrob knowrob.launch &
 
+source ${ROS_WS}/devel/setup.bash
 exec "$@"

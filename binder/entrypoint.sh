e828c32b77dc22c0019bd27eb3faf15d5eda7cb2
Srikanth Malineni <srikanthmalineni635@gmail.com>
2024-03-27 11:39:16 +0100

    combined Giskard,Knowrob Ros packages .rosinstall file

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-
-
-# Start MongoDB and save data on working directory
-MONGODB_URL=mongodb://127.0.0.1:27017
-mkdir -p ${PWD}/mongodb/data
-mongod --fork --logpath ${PWD}/mongodb/mongod.log --dbpath ${PWD}/mongodb/data
-
-# Launch Knowrob
-source ${KNOWROB_WS}/devel/setup.bash
-export KNOWROB_MONGODB_URI=${MONGODB_URL}/?appname=knowrob
-roslaunch --wait knowrob knowrob.launch &
-
 source ${ROS_WS}/devel/setup.bash
 
 roscore &
@@ -19,5 +7,17 @@ roslaunch --wait rvizweb rvizweb.launch config_file:=/home/${NB_USER}/bootstrapp
 MUJOCO_WORLD_PATH=${ROS_WS}/src/mujoco_world/mujoco_world
 mkdir ${MUJOCO_WORLD_PATH}/mujoco_world
 ln -s ${MUJOCO_WORLD_PATH}/model ${MUJOCO_WORLD_PATH}/mujoco_world/model
+
+# Start MongoDB and save data on working directory
+MONGODB_URL=mongodb://127.0.0.1:27017
+mkdir -p ${PWD}/mongodb/data
+mongod --fork --logpath ${PWD}/mongodb/mongod.log --dbpath ${PWD}/mongodb/data
+
+# Launch Knowrob
+#source ${KNOWROB_WS}/devel/setup.bash
+export KNOWROB_MONGODB_URI=${MONGODB_URL}/?appname=knowrob
+roslaunch --wait knowrob knowrob.launch &
+
+
 
 exec "$@"
